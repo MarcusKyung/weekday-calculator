da22d0eebc76a40c73f25504499f4cff7627382f
MarcusKyung <Kyungmj@gmail.com>
2023-04-12 13:22:37 -0700

    add second test for checkLeapYear()

--- __tests__/business-logic.test.js
+++ __tests__/business-logic.test.js
@@ -14,8 +14,12 @@ describe('Date', () => {
   });
 
   test('it should check if the year is a leap year' , () => {
-    let dateInLeapYear = new Date (2000, 1, 1);
+    let dateInLeapYear = new Date (2000,1,1); //this leap year is divisible by 400
+    let dateInLeapYear2 = new Date (2004,1,1); //divisible by 4 but not by 100
+    let dateNotInLeapYear = new Date (1900,1,1); //divisible by 4 and by 100, so not a leap year
     expect(dateInLeapYear.checkLeapYear()).toEqual(true);
     expect(date.checkLeapYear()).toEqual(false);
+    expect(dateInLeapYear2.checkLeapYear()).toEqual(true);
+    expect(dateNotInLeapYear.checkLeapYear()).toEqual(false);
   })
 });
--- src/js/business-logic.js
+++ src/js/business-logic.js
@@ -7,6 +7,12 @@ export class Date{
   }
 
   checkLeapYear() {
-
-  }    
+    if (this.year % 400 === 0)  {
+      return true;
+    } else if ((this.year % 4 === 0) && (this.year % 100 !== 0))  {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
